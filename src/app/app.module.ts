e933258c0fc6f0c5a18179367f4c6446e134e9e1
Natalia Fuoco <99039174+NFNat@users.noreply.github.com>
2022-09-04 22:48:38 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AboutComponent } from './components/about/about.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { TeamComponent } from './components/team/team.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+const appRoutes: Routes = [
+  { path:'home', component:HomeComponent},
+  // { path:'error', component:E404Component },
+  { path:'',  redirectTo: 'home', pathMatch:'full' },
+  // { path:'contacto', component:ContactoComponent },
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavBarComponent,
+    AboutComponent,
+    CoursesComponent,
+    TeamComponent,
+    GalleryComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
